85a2c66962a97a7d7ce1b0a579fe5191a7ebc617
Lexzino <alexudeze001@gmal.com>
2024-02-13 22:51:12 -0800

    0-list_databases.sql

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,8 +1,3 @@
-#!/bin/bash
+-- Command that lists all databases
 
-# MySQL server credentials
-MYSQL_USER="your_mysql_user"
-MYSQL_PASSWORD="your_mysql_password"
-
-# List all databases
-databases=$(mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|mysql)")
+SHOW DATABASES;
