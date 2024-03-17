cbd30cf3e72c4f72232b3666ad15fa87b573cd80
Lexzino <alexudeze001@gmal.com>
2024-03-17 02:22:33 -0700

    comment error resolved

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" script that lists all states with a name starting with N (upper N) 
+""" script that lists all states with a name starting with N (upper N)
    from the database hbtn_0e_0_usa """
 import sys
 import MySQLdb
